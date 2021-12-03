bff74efb881692a37ff9c63cefab5d49759d8e71
Dominic <dominic.gopalakrishnan@gmail.com>
2021-12-03 03:14:20 -0500

    Added additional pages and controller functions/mapping

--- recipe-project/recipe-web/src/main/java/ca/gbc/recipeproject/controllers/UserController.java
+++ recipe-project/recipe-web/src/main/java/ca/gbc/recipeproject/controllers/UserController.java
@@ -1,5 +1,7 @@
 package ca.gbc.recipeproject.controllers;
 
+import ca.gbc.recipeproject.model.Events;
+import ca.gbc.recipeproject.model.Ingredient;
 import ca.gbc.recipeproject.model.User;
 import ca.gbc.recipeproject.services.springdatajpa.IngredientSDJpaService;
 import ca.gbc.recipeproject.services.springdatajpa.MealSDJpaService;
@@ -7,7 +9,10 @@ import ca.gbc.recipeproject.services.springdatajpa.RecipeSDJpaService;
 import ca.gbc.recipeproject.services.springdatajpa.UserSDJpaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 public class UserController {
@@ -33,6 +38,29 @@ public class UserController {
         User user = userSDJpaService.findById(1L);
         model.addAttribute("ingredients", user.getShoppingList());
         return "/profile/list";
-
     }
+
+    //CREATE/SAVE
+
+    @RequestMapping({"/profile/event/create", "/profile/event/create.html"})
+    public String create(Model model) {
+        Events event = new Events();
+        User user = new User();
+        model.addAttribute("user", user);
+        model.addAttribute("event", event);
+        return "/profile/create";
+    }
+
+    @PostMapping(value = "/profile/event/save")
+    public String save(Events event, User user, Model model) {
+        event.setEventDate(new Date());
+        event.setUser(userSDJpaService.findById(1L));
+        user.getEventList().add(event);
+        userSDJpaService.findById(1L).setEventList(user.getEventList());
+        userSDJpaService.save(userSDJpaService.findById(1L));
+        model.addAttribute("event", event);
+        return "/ingredient/ingredientConfirm";
+    }
+
+
 }
